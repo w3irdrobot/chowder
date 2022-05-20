7069c80349738c75e5c0f0f8a44b9d6d6e98956b
w3irdrobot <w3irdrobot@protonmail.com>
2022-05-20 12:39:00 -0400

    Setup initial connection to Bisq APIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod bisq {
+    tonic::include_proto!("io.bisq.protobuffer");
+}
+
+use bisq::get_version_client::GetVersionClient;
+use bisq::GetVersionRequest;
+use std::time::Duration;
+use tonic::Request;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let password = std::env::var("BISQ_PASSWORD")?;
+
+    let mut client = GetVersionClient::connect("http://localhost:9998").await?;
+
+    let mut req = Request::new(GetVersionRequest {});
+    req.set_timeout(Duration::from_secs(5));
+
+    let metadata = req.metadata_mut();
+    metadata.insert("password", password.parse()?);
+
+    let response = client.get_version(req).await?.into_inner();
+    println!("bisq version: {}", response.version);
+
+    Ok(())
 }
